0b3da404aa523708a2fc6cc1aef7a7b8bb95dd8c
wisse <w.hooghiem@climotion.com>
2022-07-11 14:04:59 +0200

    Started on working new lsp installer setup

--- home/.config/nvim/lua/user/lsp/init.lua
+++ home/.config/nvim/lua/user/lsp/init.lua
@@ -4,6 +4,8 @@ if not status_ok then
 	return
 end
 
-require("user.lsp.installer")
+local servers = require("servers")
+
+require("user.lsp.installer").setup(servers.servers, options)
 require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
--- home/.config/nvim/lua/user/lsp/installer.lua
+++ home/.config/nvim/lua/user/lsp/installer.lua
@@ -1,20 +1,20 @@
-local ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not ok then
-	vim.notify("nvim-lsp-installer not found, make sure it is installed.")
-	return
+local Installer = {}
+
+function Installer.setup(servers, options)
+	local lspconfig = require("lspconfig")
+
+	-- nvim-lsp-installer must be set up before nvim-lspconfig
+	require("nvim-lsp-installer").setup({
+		ensure_installed = vim.tbl_keys(servers),
+		automatic_installation = false,
+	})
+
+	for server_name, _ in pairs(servers) do
+		local opts = vim.tbl_deep_extend("force", options, servers[server_name] or {})
+
+		-- TODO see if you need special setup for servers
+		lspconfig[server_name].setup(opts)
+	end
 end
 
--- Register a handler that will be called for all installed servers.
-lsp_installer.on_server_ready(function(server)
-	local options = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
-
-	if server.name == "sumneko_lua" then
-		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-		options = vim.tbl_deep_extend("force", sumneko_opts, options)
-	end
-
-	server:setup(options)
-end)
+return Installer
